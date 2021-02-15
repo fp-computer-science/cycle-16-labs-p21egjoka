f7461778f8650d6392c419f201229ff6cf30db96
Emanuel Gjoka <p21egjoka@fairfieldprep.org>
2021-02-14 20:40:40 -0500

    Lab 16
    Forgot to push Day 2 Labs.

---
+++ lab_2-2-.py
@@ -0,0 +1,12 @@
+# Author: Emanuel Mark Gjoka (AMDG) 2/10/2021
+
+my_file = open("alma_mater.txt", "r")
+
+contents = my_file.readlines()
+
+my_file.close()
+
+contents.reverse()
+
+for line in contents:
+    print(line)
